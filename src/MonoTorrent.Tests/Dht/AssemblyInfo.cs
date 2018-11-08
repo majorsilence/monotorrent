6dd7003777f0e332a954dd5e8a96c335dbb5a237
Peter Gill <peter@majorsilence.com>
2018-11-08 07:54:21 -0330

    Convert to new project format.
    MonoTorrent.csproj and MonoTorrent.Dht.csproj both target netstandard2.0.
    Other projects re-targeted to 4.7.1.
    
    Used tool https://github.com/hvanbakel/CsprojToVs2017 for majority of
    the conversion.

--- src/MonoTorrent.Tests/Dht/AssemblyInfo.cs
+++ src/MonoTorrent.Tests/Dht/AssemblyInfo.cs
@@ -2,10 +2,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-
-[assembly: AssemblyTitle("MonoTorrent.Dht.Tests")]
-[assembly: AssemblyProduct("MonoTorrent.Dht.Tests")]
-[assembly: AssemblyCopyright("Copyright © Alan McGovern 2008")]
 [assembly: ComVisible(false)]
 #endif
--- src/TrackerApp/AssemblyInfo.cs
+++ src/TrackerApp/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // AssemblyInfo.cs
 //
 // Authors:
@@ -28,30 +28,6 @@
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following
-// attributes.
-//
-// change them to the information which is associated with the assembly
-// you compile.
-
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all values by your own or you can build default build and revision
-// numbers with the '*' character (the default):
-
-[assembly: AssemblyVersion("1.0.*")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
